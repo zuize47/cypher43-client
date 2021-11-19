c4fb13b6d285675e1b9aa60bfc307abdd4965eb2
Hoang Nguyen <hoang.nguyen@unified.com>
2021-11-19 22:00:51 +0700

    Add loading

--- extension.js
+++ extension.js
@@ -6,30 +6,29 @@ const remove_comment_regex = /((['"])(?:(?!\2|\\).|\\.)*\2)|\/\/[^\n]*|\/\*(?:[^
 const remmove_new_line = /(\r\n|\n|\r)/gm
 const outputChannel = vscode.window.createOutputChannel(`Cypher Output`);
 
-function post_cypher(cypher, output, host) {
-	const cypher_fixed = cypher.replace(remove_comment_regex, " ") .replace(remmove_new_line, " ");	
+function post_cypher(cypher, host) {
+	const cypher_fixed = cypher.replace(remove_comment_regex, " ").replace(remmove_new_line, " ");
 	return axios.post(`${host}/db/neo4j/tx/commit`, {
 		statements: [
 			{
 				statement: cypher_fixed,
 				parameters: {}
 			}
-			]
-		})
-	  .then(function (response) {
-		outputChannel.clear()
-		output.appendLine(JSON.stringify(response.data, null, 2));
-		output.show()
-	  })
-	  .catch(function (error) {
-		vscode.window.showErrorMessage(JSON.stringify(error));
-	  });
+		]
+	});
+
 }
+let running = false;
 
 function activate(context) {
-	
+
 	const disposable = vscode.commands.registerCommand('cypher43-client.runCypher', function () {
-		
+
+		if (running) {
+			vscode.window.showWarningMessage("Job is running");
+			return
+		}
+		running = true
 		const editor = vscode.window.activeTextEditor;
 
 		if (editor) {
@@ -40,9 +39,35 @@ function activate(context) {
 				vscode.window.showErrorMessage('Add neo4j host at first line:`//neo4j:http://hostname:port`');
 				return
 			}
-			const host = matches[1] 
-			post_cypher(cypher_script, outputChannel, host)
-			
+			const host = matches[1]
+			vscode.window.withProgress({
+				location: vscode.ProgressLocation.Notification,
+				title: "Loading data!",
+			}, (progress) => {
+				let count = 1;
+				const interval = setInterval(function () {
+					progress.report({
+						increment: count < 99 ? count++ : count,
+						message: `Loading data ${count}%`
+					});
+				}, 700);
+				return post_cypher(cypher_script, host)
+					.then((response) => {
+						outputChannel.clear();
+						outputChannel.appendLine(JSON.stringify(response.data, null, 2));
+						outputChannel.show();
+					})
+					.catch(error => {
+						vscode.window.showErrorMessage(JSON.stringify(error));
+					})
+					.finally(() => {
+						running = false
+						progress.report({ increment: 100 });
+						clearInterval(interval)
+					})
+			});
+
+
 		}
 	});
 
@@ -51,7 +76,7 @@ function activate(context) {
 }
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
